8e386ee5a66aa1c0fd5e5cc773962ea3d53a4e01
Soumyajyoti Dey <deysoumyajyoti2017@gmail.com>
2023-07-12 00:14:25 +0530

    fix right sidebar component

--- client/my-app/src/App.js
+++ client/my-app/src/App.js
@@ -1,28 +1,22 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import './App.css';
-import Navbar from './components/Navbar'
-import AllRoutes from './AllRoutes'
-// import { Routes, Route} from 'react-router-dom'
-import Home from './pages/Home/Home'
-import Auth from './pages/Auth/Auth'
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home/Home";
+import Auth from "./pages/Auth/Auth";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <Navbar />
-      {/* <AllRoutes/> */}
-      <Routes>
-        <Route  path='/' element={<Home/>}/>
-        <Route path='/Auth' element={<Auth/>}/>
-
-        
-    </Routes>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Auth" element={<Auth />} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
 }
 
 export default App;
-
---
+++ client/my-app/src/components/RightSidebar/RightSidebar.jsx
@@ -0,0 +1,5 @@
+const RightSidebar = () => {
+  return <></>;
+};
+
+export default RightSidebar;
